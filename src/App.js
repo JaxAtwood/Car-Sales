c872d7ee7c198a3af3c4fd24eec242e02801dd81
Jackie Atwood <takalaj@verizon.net>
2019-10-10 21:48:05 -0400

    undefined state?

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import Header from './components/Header';
 import AddedFeatures from './components/AddedFeatures';
 import AdditionalFeatures from './components/AdditionalFeatures';
 import Total from './components/Total';
-
+import { connect } from "react-redux";
+import { addFeature, removeFeature } from "./actions/index";
 
 
 //state is an object
@@ -17,30 +18,17 @@ import Total from './components/Total';
 //then update car property, spread in all car properties
 //then update features, then spread in all current features
 //then add feature
-const App = () => {
-  const state = {
-    additionalPrice: 0,
-    car: {
-      price: 26395,
-      name: '2019 Ford Mustang',
-      image:
-        'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
-      features: []
-    },
-    store: [
-      { id: 1, name: 'V-6 engine', price: 1500 },
-      { id: 2, name: 'Racing detail package', price: 1500 },
-      { id: 3, name: 'Premium sound system', price: 500 },
-      { id: 4, name: 'Rear spoiler', price: 250 }
-    ]
-  };
+const App = (props) => {
+  
 
   const removeFeature = item => {
     // dispatch an action here to remove an item
+    props.removeFeature(item);
   };
 
   const buyItem = item => {
     // dipsatch an action here to add an item
+    props.addFeature(item);
   };
 
   return (
@@ -56,8 +44,15 @@ const App = () => {
     </div>
   );
 };
+const mapStateToProps = state => {
+  return {
+    car: state.car, 
+    store: state.store,
+    additonalPrice: state.additionalPrice
+  }
+}
 
-export default App;
+export default connect(mapStateToProps, {addFeature, removeFeature})(App)
 
 /*
 //import connect from react-redux
--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 import App from './App';
-
+import { reducer } from "./reducers/Reducer";
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+const store = createStore(reducer);
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+<Provider store={store}>
+<App />
+</Provider>
+, rootElement);
 
 /*
 //here you'll want to import {createStore} from redux;
